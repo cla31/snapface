adb233e5f0ea9e35581e3674c5a34d66b44e1803
Claire_M <clairerodriguez79@yahoo.fr>
2023-04-02 18:39:17 +0200

    Premier composant stylisé, avec données affichées.

---
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+// Pour initialiser ces propriétés en suivant 
+// les best practices Angular, vous allez utiliser la méthode  ngOnInit(). 
+// Pour l'utiliser, votre component doit implémenter l'interface  OnInit
+export class FaceSnapComponent implements OnInit {
+  // Pour éviter que typescrit râle on ajoute un bind (le point d'exclamation)
+title!:string;
+description!:string;
+createDate!:Date;
+//le nombre de likes
+snaps!:number;
+imageUrl!:string;
+
+//C'est ici qu'on initialise les propriétés
+//On prépare les données
+ngOnInit(){
+this.title="Archibald";
+this.description="Mon meilleur ami depuis tout petit !";
+this.createDate= new Date();
+this.snaps=6;
+this.imageUrl='https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+}
+}
