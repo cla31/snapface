91d59a634c6715d4269256759840da29ab375b0c
Claire_M <clairerodriguez79@yahoo.fr>
2023-04-05 08:58:42 +0200

    Propriétés personnalisées

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-}
+export class AppComponent implements OnInit {
+  mySnap!:FaceSnap;
+  myOtherSnap!:FaceSnap;
+  myLastSnap!:FaceSnap;
+
+  ngOnInit(){
+    this.mySnap = new FaceSnap(
+      "Archibald",
+      "Mon meilleur ami depuis tout petit !",
+      new Date(),
+      0,
+      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+    );
+    this.myOtherSnap = new FaceSnap(
+      "Three Rock Mountain",
+      "Un endroit magnifique pour les randonnées.",
+      new Date(),
+      0,
+    'https://upload.wikimedia.org/wikipedia/commons/0/08/Three_Rock_Mountain_Southern_Tor.jpg'
+    );
+    this.myLastSnap = new FaceSnap(
+      "Un bon repas",
+      "Mmmh que c\'est bon!",
+      new Date(),
+      0,
+    'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg')
+  }  
+};
+
--- snapface/src/app/face-snap/face-snap.component.ts
+++ snapface/src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
@@ -8,37 +9,26 @@ import { Component, OnInit } from '@angular/core';
 // les best practices Angular, vous allez utiliser la méthode  ngOnInit(). 
 // Pour l'utiliser, votre component doit implémenter l'interface  OnInit
 export class FaceSnapComponent implements OnInit {
-  // Pour éviter que typescrit râle on ajoute un bind (le point d'exclamation)
-title!:string;
-description!:string;
-createDate!:Date;
-//le nombre de likes
-snaps!:number;
-imageUrl!:string;
-buttonText!:string;
+  //Un autre décorateur
+  @Input()faceSnap!:FaceSnap;
+  buttonText!:string;
 
-//C'est ici qu'on initialise les propriétés
-//On prépare les données
 ngOnInit(){
-this.title="Archibald";
-this.description="Mon meilleur ami depuis tout petit !";
-this.createDate= new Date();
-this.snaps=6;
-this.imageUrl='https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
 this.buttonText='Oh Snap!!';
 }
 
 onSnap(){
 if(this.buttonText==='Oh Snap!!'){
   //L'utilisateur n'a pas encore cliqué sur le bouton
-  this.snaps++;
+  this.faceSnap.snaps++;
   this.buttonText='Oups! unSnap!!';
 }
 else{
   // Si on est déjà sur unSnap
-  this.snaps--;
+  this.faceSnap.snaps--;
   this.buttonText='Oh Snap!!';
 }
 }
+//Ecrire d'autres snaps!!! (cf fin dernière vidéo!!!)
 
 }
